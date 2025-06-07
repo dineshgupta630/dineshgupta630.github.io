f0c3e50d183ff1a2c0b032b3d9b05ce39b28dab0
Dinesh Gupta <dineshgupta@Dineshs-MacBook-Air.local>
2025-06-07 19:06:22 +0100

    Changing repo name

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 import createMDX from '@next/mdx';
 
-const repo = 'dineshgupta630';
+const repo = 'showcase';
 const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
